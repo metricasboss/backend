5be36f549610721dade0ff919c190dd1e0fa10c5
Lucian Fialho <lucianfialhobp@hotmail.com>
2020-06-29 11:18:00 -0300

    -- enxugando arquivo de configuracao do banco

--- config/database.js
+++ config/database.js
@@ -4,16 +4,6 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-      },
-      options: {
-        useNullAsDefault: true,
-      },
-    },
-    production: {
-      connector: 'bookshelf',
-      settings: {
         client: 'postgres',
         host: env('DATABASE_HOST', '127.0.0.1'),
         port: env.init('DATABASE_PORT', 5432),
@@ -22,6 +12,7 @@ module.exports = ({ env }) => ({
         password: env('DATABASE_PASSWORD', ''),
       },
       options: {
+        useNullAsDefault: true,
         ssl: false,
       },
     },
