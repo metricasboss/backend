224c594cc537103939b20e4b749a4b09d74d62b9
Lucian Fialho <lucianfialhobp@hotmail.com>
2020-06-29 11:04:54 -0300

    -- adicionando configuracao do banco

--- config/database.js
+++ config/database.js
@@ -11,5 +11,19 @@ module.exports = ({ env }) => ({
         useNullAsDefault: true,
       },
     },
+    production: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'postgres',
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.init('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', ''),
+        password: env('DATABASE_PASSWORD', ''),
+      },
+      options: {
+        ssl: false,
+      },
+    },
   },
 });
